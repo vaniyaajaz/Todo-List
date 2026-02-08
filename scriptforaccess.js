b6e6340044e121cf1c1004963d6b7e7d05c18043
vaniyaajaz <vaniya.ajaz@pakturkmaarif.edu.pk>
2026-02-08 18:05:19 +0500

    Update scriptforaccess.js

--- scriptforaccess.js
+++ scriptforaccess.js
@@ -1,8 +1,26 @@
-window.location.href = "index.html"
-password = window.prompt("Password?")
+const pass_show = document.querySelector("#hid")
+const textbox = document.getElementById("password")
+const overwrite = document.getElementById("overcome")
 
-if (password === 'invisiblestring') {
-    window.location.href = "AccessPage.html"
-} else {
-    window.location.href = "wrong.html"
+pass_show.addEventListener("click", () => {
+    if (textbox.type === "password") {
+        textbox.type = "text"
+        pass_show.id = "shown"
+    } else {
+        textbox.type = "password"
+        pass_show.id = "hid"
+    }
+})
+
+function checkpassword() {
+    const password = textbox.value
+    if (password === 'invisiblestring') {
+        window.location.href = "AccessPage.html"
+    } else {
+        window.location.href = "wrong.html"
+    }
 }
+
+overwrite.addEventListener("click", () => {
+    window.location.href = "AccessPage.html"
+})
